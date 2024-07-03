2a1c275df5972695c1def43936907f3d42b80540
Abbat1 <cozzyyist@gmail.com>
2024-07-03 08:01:51 -0500

    java303.2

---
+++ src/coreJava.java
@@ -0,0 +1,34 @@
+public class coreJava {
+    public static void main(String[] args) {
+        // above is the first bullet point
+        int a, b = 20, c = 40;
+        // equation to add the both together
+        //X = X+Y
+        a = b + c;
+        System.out.println("" + a);
+        // below is the second bullet point.
+          double g = 7.75;
+          double e = 9.95;
+
+          double x = e + g ;
+        System.out.println("" +x);
+        //third bullet point
+        int u = 15;
+        double z = 128.4;
+        double sum = u + z;
+        System.out.println(sum);
+
+        //fourth bullet point
+        int q, n = 12, p =96;
+        //below is the equation to divide the bigger number by the smaller number
+        q = p / n;
+        System.out.println(q);
+        //below is the fifth bullet point
+        double L = 55.5;
+        double M = 550.5;
+        //here is where we print the answer
+        double N = M / L;
+        System.out.println(n);
+        System.out.println(n);
+    }
+}
